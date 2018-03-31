9d22ee635ddf080068d0ee0f893d7596e7106f07
yiyupan <yiyupan@umich.edu>
2018-03-31 15:53:44 -0400

    update external ip

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.184.34.96:9200"
+	ES_URL = "http://35.192.42.185:9200"
 )
 
 func main() {
